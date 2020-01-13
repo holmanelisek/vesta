10a3bf0c8a8bce34f06aba2543cc76cd61bb4053
Dalton Maurer <dmaurer3394@gmail.com>
2020-01-13 13:55:47 -0500

    'npm start' command starts server and react

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
-// Requiring passport as we've configured it
+var routes = require("./routes");
 var passport = require("./config/passport");
 
-// Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3001;
 var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/public"));
+}
+
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
@@ -21,8 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+app.use(routes);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
