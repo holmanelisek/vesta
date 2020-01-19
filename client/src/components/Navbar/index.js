e1c969f920ebb5fd647d7f37291172ad00d31403
Mao <lin.maoxian0991@gmail.com>
2020-01-19 15:16:34 -0500

    Vesat.js added react-bootstrap import for react modals, added code toggle modal based on sign in or sign up. SignIn and Signup components, removed jsx code for modals and left only input jsx pets component, changed class attribute for the img tag to className Navbar component, changed this component to a functional component and removed the switch component along with route, menu will be returned based on some conditions

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -5,65 +5,7 @@ import HomeNav from "./nav-comp/homenav";
 import API from "../../utils/API";
 import "./style.css";
 
-class Navbar extends Component{
-    state = {
-        isSignedIn: false,
-        data: []
-    };
-
-    handleSignOutSubmit = event => {
-        event.preventDefault();
-        API.signOut()
-          .then( res => {
-                window.location.reload();
-            })
-      }
-
-    //Function for displaying menu links based on if user is logged in or not.
-    isSignedIn = () => {
-        console.log(this.props.authenticated)
-        console.log(this.props);
-        if(this.props.authenticated){
-            return (
-                <ul className="navbar-nav text-uppercase ml-auto">
-                    <Switch>
-                        <Route path="/" exact render={props => (<HomeNav onClickHome = {()=> this.props.history.push("/")}/>)}/>
-                        <Route path="/homehub" exact render={props => (<HomeHubNav onClickHome = {()=> this.props.history.push("/")}/>)}/>
-                    </Switch>
-                    <li className="nav-item">
-                        {/* Figure out how to sign users out */}
-                        <a className="nav-link" href="#" onClick={this.handleSignOutSubmit} >Sign Out</a>
-                    </li>
-                </ul>
-            )
-        //If res.email is not true render this menu
-        }else{
-            return (
-                <ul className="navbar-nav text-uppercase ml-auto">
-                    <li className="nav-item">
-                        {<Link to="/" className="nav-link">Home</Link>}
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#about">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link " href="#services">Features</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#team">Team</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#" data-toggle="modal" data-target="#modalLoginForm">Sign In</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#" data-toggle="modal" data-target="#modalRegisterForm" >Sign Up</a>
-                    </li>
-                </ul>
-            )
-        }
-    }
-    
-    render() {
+function Navbar (props){
         return (
             <nav className="navbar navbar-expand-lg navbar-dark fixed-top nav-color" id="homeNavBar">
                 <div className="container">
@@ -72,12 +14,64 @@ class Navbar extends Component{
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        {this.isSignedIn()}
+                            {props.authenticated ?
+                                    props.history.location.pathname === "/" ? 
+                                        <ul className="navbar-nav text-uppercase ml-auto">
+                                            <li className="nav-item">
+                                                <a className="nav-link" href="#page-top">Home</a>
+                                            </li>
+                                            <li className="nav-item">
+                                                <a className="nav-link" href="#about">About</a>
+                                            </li>
+                                            <li className="nav-item">
+                                                <a className="nav-link " href="#services">Features</a>
+                                            </li>
+                                            <li className="nav-item">
+                                                <a className="nav-link" href="#team">Team</a>
+                                            </li>
+                                            <li className="nav-item">
+                                                {<Link to="/Homehub" className="nav-link">Home Hub</Link>}
+                                            </li>
+                                            <li className="nav-item">
+                                                <a className="nav-link" href="#" onClick={props.clickSignout} >Sign Out</a>
+                                            </li>
+                                        </ul>
+                                    :props.history.location.pathname === "/Homehub" ?
+                                            <ul className="navbar-nav text-uppercase ml-auto">
+                                                <li className="nav-item">
+                                                    {<Link to="/" className="nav-link">Home</Link>}
+                                                </li>
+                                                <li className="nav-item">
+                                                    <a className="nav-link" href="#" onClick={props.clickSignout} >Sign Out</a>
+                                                </li> 
+                                            </ul>
+                                    :null
+                            : 
+                                <ul className="navbar-nav text-uppercase ml-auto">
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#page-top">Home</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#about">About</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link " href="#services">Features</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#team">Team</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="" onClick={props.clickModalSignIn}>Sign In</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#" onClick={props.clickModalSignUp}>Sign Up</a>
+                                    </li>
+                                </ul>
+                            }
                     </div>
                 </div>
             </nav>
         );
     }
-}
 
 export default withRouter(Navbar);
