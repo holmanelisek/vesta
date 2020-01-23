ea40595e3654f36fb695c97ec8f669e0c67e3821
Mao Lin <lin.maoxian0991@gmail.com>
2020-01-23 12:56:05 -0500

    Add new vet function, route, and components are complete

--- client/src/components/Pets/index.js
+++ client/src/components/Pets/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import {Modal} from 'react-bootstrap';
 import DeletePet from "../DeletPet";
-import {SubmitPet, AddPetModal} from "../SubmitPet";
 import {NewPetForm, NewPetTitle } from "../NewPetForm";
 import API from "../../utils/API"
 
