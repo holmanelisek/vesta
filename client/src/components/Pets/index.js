6c28ed70de438828b08ffca6a66a7f563ea9ccc6
Mao <lin.maoxian0991@gmail.com>
2020-01-23 00:17:59 -0500

    Add pet deletion function, routes, and button. Also additional changes to how home home saves states and passes states

--- client/src/components/Pets/index.js
+++ client/src/components/Pets/index.js
@@ -1,33 +1,27 @@
 import React, { Component } from "react";
-import Modal from 'react-modal';
-
-const customStyles = {
-  content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)'
-  }
-}
+import {Modal} from 'react-bootstrap';
+import DeletePet from "../DeletPet";
+import API from "../../utils/API"
 
 class Pets extends Component{
-      constructor() {
-        super();
 
-        this.state = {
-            modalIsOpen: false,
-            users: []
-        };
-
-        this.openModal = this.openModal.bind(this);
-        this.afterOpenModal = this.afterOpenModal.bind(this);
-        this.closeModal = this.closeModal.bind(this);
+    state = {
+        modalShow: false,
     }
 
-    openModal() {
-        this.setState({ modalIsOpen: true });
+    deletePet = (petId, admin, user) => {
+      if(admin === user){
+        console.log("All dogs go to heaven " + petId)
+        API.removePet(petId)
+          .then(response => {
+            console.log(response.data);
+            this.props.getPetData(this.props.home_id);
+          }).catch()
+      }
+    }
+
+    openModal = () => {
+        this.setState({ modalShow: true});
     }
 
     afterOpenModal() {
@@ -35,42 +29,60 @@ class Pets extends Component{
         // this.subtitle.style.color = '#f00';
     }
 
-    closeModal() {
-        this.setState({ modalIsOpen: false });
+    closeModal = () => {
+        this.setState({ modalShow: false });
     }
  
+    adminFunctionDeletePet = (admin, user) => {
+      console.log(this.props)
+      if(admin === user){
+        return (
+          <DeletePet 
+            clickFunc = {() => this.deletePet(this.props.pet.id, admin, user)}
+          />
+          )
+      }else{
+        return null
+      }
+    }
+
     render() {
-    return (
-      <div className="col-sm-3">
-        <div className="card">
-          <img src={'https://i.pinimg.com/originals/ae/c4/53/aec453161b2f33ffc6219d8a758307a9.jpg'} className="card-img-top img-responsive" alt="Cute Puppy" />
-          <div className="card-body">
-            <h5 className="card-title">{this.props.pet.pet_name}</h5>
-            <p className="card-text">Age:{this.props.pet.age}</p>
-            <p className="card-text">Primary Vet: {this.props.pet.primary_vet_id}</p>
-            <p className="card-text">Emergency Vet: {this.props.pet.emergency_vet_id}</p>
-            <p className="card-text">Pets description</p>
-            <button onClick={this.openModal} className="btn btn-primary">More information</button>
+      return (
+        <div className="col-sm-3">
+          <div className="card">
+            <img src={'https://i.pinimg.com/originals/ae/c4/53/aec453161b2f33ffc6219d8a758307a9.jpg'} className="card-img-top img-responsive" alt="Cute Puppy" />
+            <div className="card-body">
+              <h5 className="card-title">{this.props.pet.pet_name}</h5>
+              <p className="card-text">Age:{this.props.pet.age}</p>
+              <button onClick={this.openModal} className="btn btn-primary">More information</button>
+            </div>
           </div>
+          <Modal show={this.state.modalShow} onHide={this.closeModal} backdrop='static'>
+            <Modal.Title>
+                <div className="">
+                    <h2 ref={subtitle => this.subtitle = subtitle}>{this.props.pet.pet_name} <span className="float-right">{this.adminFunctionDeletePet(this.props.home_admin, this.props.user)}</span></h2>
+                </div>
+            </Modal.Title>
+            <Modal.Body>
+              <div>
+                <p>Pet Name: {this.props.pet.pet_name}</p>
+                <p>Pet Aage: {this.props.pet.age}</p>
+                <hr/>
+                <p>Primary Vet: {this.props.pet.primary_vet_info.practice_name}</p>
+                <p>Phone Number: {this.props.pet.primary_vet_info.phone_number}</p>
+                <p>Address: {this.props.pet.primary_vet_info.street}, {this.props.pet.primary_vet_info.city}, {this.props.pet.primary_vet_info.state} {this.props.pet.primary_vet_info.zip}</p>
+                <hr/>
+                <p className="card-text">Pets description</p>
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <div>
+                <button type="button" className="btn btn-secondary" onClick={this.closeModal}>Close</button>
+              </div>
+            </Modal.Footer>
+          </Modal>
         </div>
-
-        <Modal
-            isOpen={this.state.modalIsOpen}
-            onAfterOpen={this.afterOpenModal}
-            onRequestClose={this.closeModal}
-            style={customStyles}
-            contentLabel="Example Modal"
-        >
-
-            <h2 ref={subtitle => this.subtitle = subtitle}>{this.props.pet.pet_name}</h2>
-            <hr />
-            <p>Pet Name: {this.props.pet.pet_name}</p>
-            <p>Pet Aage: {this.props.pet.age}</p>
-            <p>Primary Vet: {this.props.pet.primary_vet_id}</p>
-            <button type="button" className="btn btn-secondary" onClick={this.closeModal}>Close</button>
-        </Modal>
-      </div>
-    )};
+      )};
 }
 
 export default Pets
